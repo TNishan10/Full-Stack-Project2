c5f70fae7b28c613bd823a0dd20ca54f60b38b66
Nishan Thapa <tnishan110@gmail.com>
2024-11-21 16:21:31 +0545

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div><Navbar />
+        <div>body</div>
+        <div><Footer /></div>
+    </div>
+    
+  )
+}
+
+export default Home
